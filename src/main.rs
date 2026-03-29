4bb45f87c63e188e2f3ad8741dd15909b53f4e9d
Rafael Bachmann <rafael.bachmann@paessler.com>
2026-03-29 13:48:52 +0200

    chore: run RustFmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::collections::BTreeMap;
 use futures::{SinkExt, StreamExt};
 use openvpn_mgmt_codec::{
     OvpnCodec, OvpnCommand,
     stream::{ManagementEvent, classify},
 };
+use std::collections::BTreeMap;
 use tokio::net::{TcpListener, TcpStream, UnixStream};
 use tokio_util::codec::Framed;
 use tracing::{debug, error, info};
@@ -14,7 +14,6 @@ async fn main() -> anyhow::Result<()> {
     let mut framed = Framed::new(stream, OvpnCodec::new());
     let (mut sink, raw_stream) = framed.split();
     let mut mgmt = raw_stream.map(classify);
-
 
     while let Some(event) = mgmt.next().await {
         match event? {
@@ -30,22 +29,26 @@ async fn main() -> anyhow::Result<()> {
                     ..
                 } = &notification
                 {
-
-                    let env = BTreeMap::from_iter(env.into_iter().map(|(k, v)| (k.to_lowercase(), v)));
-                    if let Some(password) = env.get("password") && let Some(username) = env.get("username") {
+                    let env =
+                        BTreeMap::from_iter(env.into_iter().map(|(k, v)| (k.to_lowercase(), v)));
+                    if let Some(password) = env.get("password")
+                        && let Some(username) = env.get("username")
+                    {
                         info!(%cid, %password, "auto-approving client");
 
                         if (password.as_str() == "pass123" && username.as_str() == "user")
-                            || password.as_str() == "123pass" && username.as_str() == "client-with-certs" {
-
-
-                            if let Some(commonName) = env.get("common_name") && commonName == username {
+                            || password.as_str() == "123pass"
+                                && username.as_str() == "client-with-certs"
+                        {
+                            if let Some(commonName) = env.get("common_name")
+                                && commonName == username
+                            {
                                 sink.send(OvpnCommand::ClientAuthNt {
                                     cid: *cid,
                                     kid: *kid,
                                 })
-                                    .await?;
-                                continue
+                                .await?;
+                                continue;
                             }
                             sink.send(OvpnCommand::ClientDeny {
                                 cid: *cid,
@@ -55,7 +58,6 @@ async fn main() -> anyhow::Result<()> {
                                 client_reason: Some(String::from("CRV1:R,E:T20wMXU3Rmg0THJHQlM3dWgwU1dtendhYlVpR2lXNmw=:dXNlcg==:Enter Your OTP Code")),
                             })
                                 .await?;
-
                         } else {
                             sink.send(OvpnCommand::ClientDeny {
                                 cid: *cid,
@@ -63,7 +65,7 @@ async fn main() -> anyhow::Result<()> {
                                 reason: String::from("Invalid user/password"),
                                 client_reason: None,
                             })
-                                .await?;
+                            .await?;
                         }
                     }
                 }
